0de043cf6db67ca4bcf214ad8119aece46a1b7cf
benisyke <shimazawa_takumi@koushi-intec.co.jp>
2025-04-14 16:02:55 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,27 @@
+package kadai_018;
+
+//抽象クラス
+abstract public class Kato_Chapter18 {
+	
+	public String familyName = "加藤"; //姓
+	public String givenName;  //名
+	public String address = "東京都中野区〇×";  // 住所を東京都中野区〇×に設定;    //住所
+	
+	public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+	
+	//共通の紹介
+	public void commonIntroduce(String familyName,String givenName,String address) {
+		System.out.println("名前は"+familyName+givenName+"です");
+		System.out.println("住所は"+address+"です");
+	}
+	//抽象メソッド 個別の紹介
+	abstract public void eachIntroduce();
+	
+	////紹介の実行
+	public void execIntroduce() {
+		commonIntroduce(familyName,givenName,address);
+		eachIntroduce();
+	}
+}
